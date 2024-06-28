00c68628163396dd65e1fdb5c93af6acf8598c81
karthik <karthiknadar1204@gmail.com>
2024-06-28 15:50:22 +0530

    basic setup prep

---
+++ Interfaces/Interface.ts
@@ -0,0 +1,34 @@
+interface User {
+    readonly dbId: number
+    email: string,
+    userId: number,
+    googleId?: string
+    // startTrail: () => string
+    startTrail(): string   // basically a function returning a string.
+    getCoupon(couponname: string, value: number): number
+}
+
+
+//************** REOPENING OF INTERFACE*************** */
+interface User {
+    githubToken: string
+}
+
+interface Admin extends User {
+    role: "admin" | "ta" | "learner"
+}
+
+//************************************************** */
+
+const hitesh: Admin = { dbId: 22, email: "h@h.com", userId: 2211,
+role: "admin",
+githubToken: "github",
+startTrail: () => {
+    return "trail started"
+},
+getCoupon: (name: "hitesh10", off: 10) => {
+    return 10
+}
+}
+hitesh.email = "h@hc.com"
+// hitesh.dbId = 33
